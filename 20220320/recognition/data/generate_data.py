0db6a5445731f6a5ad2b43e7b74f7b2cb76b9158
jesunglim <jesunglimkorea@gmail.com>
2022-03-20 19:47:02 +0900

    insightface

---
+++ 20220320/recognition/data/generate_data.py
@@ -0,0 +1,249 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import os
+
+import numpy as np
+import tensorflow as tf
+
+import sys
+sys.path.append("..")
+from predict import *
+
+#tf.enable_eager_execution()
+
+
+class GenerateData:
+
+    def __init__(self, config=None):
+        self.config = config
+        self.train_paths, self.train_labels = self._get_path_label(self.config['train_dir'])
+        self.valid_paths, _ = self._get_path_label(self.config['valid_dir'])
+
+    @staticmethod
+    def _get_path_label(image_dir):
+        image_dir = os.path.expanduser(image_dir)
+        ids = list(os.listdir(image_dir))
+        ids.sort()
+        cat_num = len(ids)
+        # logger.info("the total people number is {}".format(cat_num))
+        id_dict = dict(zip(ids, list(range(cat_num))))
+        paths = []
+        labels = []
+        for i in ids:
+            cur_dir = os.path.join(image_dir, i)
+            fns = os.listdir(cur_dir)
+            paths.append([os.path.join(cur_dir, fn) for fn in fns])
+            labels.append([id_dict[i]] * len(fns))
+        return paths, labels
+
+    def _preprocess(self, image_path, training=True):
+        image_raw = tf.io.read_file(image_path)
+        # image = tf.image.decode_image(image_raw)
+        image = tf.image.decode_png(image_raw)
+        image = tf.cast(image, tf.float32)
+        image = image / 255
+        image = tf.image.resize(image, (self.config['image_size'], self.config['image_size']))
+
+        # image = tf.image.resize(image, (224, 224))
+        # image = tf.image.random_crop(image, size=[112, 112, 3])
+        # image = tf.image.random_flip_left_right(image)
+
+        # image = image[None, ...]
+        return image
+
+    def _preprocess_train(self, image_path, label):
+        image = self._preprocess(image_path, training=True)
+
+        return image, label
+
+    def _preprocess_train_triplet(self, image_path1, image_path2, image_path3):
+        image1 = self._preprocess(image_path1, training=True)
+        image2 = self._preprocess(image_path2, training=True)
+        image3 = self._preprocess(image_path3, training=True)
+
+        return image1, image2, image3
+
+    def _preprocess_val(self, image_path1, image_path2, label):
+        image1 = self._preprocess(image_path1, training=False)
+        image2 = self._preprocess(image_path2, training=False)
+
+        return image1, image2, label
+
+    def get_train_data(self):
+        paths, labels = self.train_paths, self.train_labels
+        cat_num = len(paths)
+        paths = [path for cls in paths for path in cls]
+        labels = [label for cls in labels for label in cls]
+        assert (len(paths) == len(labels))
+        total = len(paths)
+        # logger.info("the total pic number is {}".format(total))
+        train_dataset = tf.data.Dataset.from_tensor_slices((paths, labels))
+        train_dataset = train_dataset.cache()
+        train_dataset = train_dataset.shuffle(total)
+        train_dataset = train_dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+        train_dataset = train_dataset.map(self._preprocess_train, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        train_dataset = train_dataset.batch(self.config['batch_size'])
+
+        return train_dataset, cat_num
+
+    def get_train_triplets_data(self, model):
+        paths, labels = self.train_paths, self.train_labels
+        begins = []  # include
+        ends = []  # not include
+        length = 0
+        for l in labels:
+            for _ in l:
+                begins.append(length)
+                ends.append(length + len(l))
+            length += len(l)
+
+        paths = [path for cls in paths for path in cls]
+        labels = [label for cls in labels for label in cls]
+        assert (len(paths) == len(labels))
+
+        train_dataset = tf.data.Dataset.from_tensor_slices((paths, labels))
+        train_dataset = train_dataset.cache()
+        train_dataset = train_dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+        train_dataset = train_dataset.map(self._preprocess_train, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        train_dataset = train_dataset.batch(self.config['batch_size'])
+
+        embs = None
+        labels = None
+        for img, label in train_dataset:
+            emb = get_embeddings(model, img)
+            if embs is None:
+                embs = emb
+                labels = label
+            else:
+                embs = tf.concat([embs, emb], axis=0)
+                labels = tf.concat([labels, label], axis=0)
+
+        num_triplets = 0
+        anchor = []
+        pos = []
+        neg = []
+        for a_idx in range(labels.shape[0]):
+            begin = begins[a_idx]  # include
+            end = ends[a_idx]  # not include
+            neg_dists = np.sum(np.square(embs - embs[a_idx]), axis=1)
+            # neg_dists[begin:end] = np.NaN
+            neg_dists[begin:end] = np.Inf
+            for p_idx in range(a_idx + 1, end):
+                pos_dist = np.sum(np.square(embs[p_idx] - embs[a_idx]))
+                all_neg = np.where(neg_dists - pos_dist < self.config['alpha'])[0]
+                num_neg = all_neg.shape[0]
+                if num_neg > 0:
+                    rnd_idx = np.random.randint(num_neg)
+                    n_idx = all_neg[rnd_idx]
+
+                    anchor.append(paths[a_idx])
+                    pos.append(paths[p_idx])
+                    neg.append(paths[n_idx])
+
+                    num_triplets += 1
+        if num_triplets > 0:
+            train_dataset = tf.data.Dataset.from_tensor_slices((anchor, pos, neg))
+            train_dataset = train_dataset.cache()
+            train_dataset = train_dataset.shuffle(num_triplets)
+            train_dataset = train_dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+            train_dataset = train_dataset.map(self._preprocess_train_triplet,
+                                              num_parallel_calls=tf.data.experimental.AUTOTUNE)
+            train_dataset = train_dataset.batch(self.config['batch_size'])
+
+        return train_dataset, num_triplets
+
+    def get_val_data(self, num):
+        paths = self.valid_paths
+        paths1 = []
+        paths2 = []
+        labels = []
+        oo = 0
+        while oo < num / 2:
+            num_cls = np.random.randint(0, len(paths))
+            cls = paths[num_cls]
+            if len(cls) > 0:
+                im_no1 = np.random.randint(0, len(cls))
+                im_no2 = np.random.randint(0, len(cls))
+                if im_no1 != im_no2:
+                    paths1.append(cls[im_no1])
+                    paths2.append(cls[im_no2])
+                    labels.append(True)
+                    oo = oo + 1
+
+        nn = 0
+        while nn < num / 2:
+            num_cls1 = np.random.randint(0, len(paths))
+            num_cls2 = np.random.randint(0, len(paths))
+            if num_cls1 != num_cls2:
+                cls1 = paths[num_cls1]
+                cls2 = paths[num_cls2]
+                if len(cls1) > 0 and len(cls2) > 0:
+                    im_no1 = np.random.randint(0, len(cls1))
+                    im_no2 = np.random.randint(0, len(cls2))
+                    paths1.append(cls1[im_no1])
+                    paths2.append(cls2[im_no2])
+                    labels.append(False)
+                    nn = nn + 1
+
+        val_dataset = tf.data.Dataset.from_tensor_slices((paths1, paths2, labels))
+        val_dataset = val_dataset.cache()
+        val_dataset = val_dataset.shuffle(num)
+        val_dataset = val_dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+        val_dataset = val_dataset.map(self._preprocess_val, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        val_dataset = val_dataset.batch(self.config['valid_batch_size'])
+        return val_dataset
+
+
+def parse_args(argv):
+    import argparse
+    parser = argparse.ArgumentParser(description='Generate Data.')
+    parser.add_argument('--config_path', type=str, help='path to config path', default='../configs/config.yaml')
+    args = parser.parse_args(argv)
+
+    return args
+
+
+def main():
+    import sys
+    args = parse_args(sys.argv[1:])
+    # logger.info(args)
+    import yaml
+    with open(args.config_path) as cfg:
+        config = yaml.load(cfg, Loader=yaml.FullLoader)
+    gd = GenerateData(config)
+    # train_data, classes = gd.get_train_data()
+    import matplotlib.pyplot as plt
+    # for img, _ in train_data.take(1):
+    #     plt.imshow(img[0])
+    #     plt.show()
+
+    # val_data = gd.get_val_data(3)
+    # for img1, img2, label in val_data:
+    #     print(label)
+    #     plt.imshow(img1[0])
+    #     plt.show()
+    #
+    #     plt.imshow(img2[0])
+    #     plt.show()
+    from recognition.backbones.resnet_v1 import ResNet_v1_50
+    from recognition.models.models import MyModel
+    model = MyModel(ResNet_v1_50, embedding_size=config['embedding_size'])
+    triplet_data, _ = gd.get_train_triplets_data(model)
+    for img1, img2, img3 in triplet_data.take(1):
+        plt.imshow(img1[0])
+        plt.show()
+
+        plt.imshow(img2[0])
+        plt.show()
+        plt.imshow(img3[0])
+        plt.show()
+
+
+if __name__ == '__main__':
+    # log_cfg_path = '../../logging.yaml'
+    # with open(log_cfg_path, 'r') as f:
+    #     dict_cfg = yaml.load(f, Loader=yaml.FullLoader)
+    # logging.config.dictConfig(dict_cfg)
+    # logger = logging.getLogger("mylogger")
+    # logger.info("hello, insightface/recognition")
+    main()
---
+++ 20220320/recognition/train.py
@@ -0,0 +1,227 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import argparse
+import datetime
+import os
+import platform
+import sys
+import time
+
+import tensorflow as tf
+import yaml
+
+from backbones.resnet_v1 import ResNet_v1_50
+from data.generate_data import GenerateData
+from losses.loss import arcface_loss, triplet_loss, center_loss
+from models.models import MyModel
+from predict import get_embeddings
+from valid import Valid_Data
+
+# os.environ['CUDA_VISIBLE_DEVICES'] = "2,3"
+# config = tf.ConfigProto()
+# config.gpu_options.allow_growth = True
+# config.gpu_options.per_process_gpu_memory_fraction = 0.4
+# tf.enable_eager_execution(config=config)
+
+#tf.enable_eager_execution()
+
+
+# log_cfg_path = '../logging.yaml'
+# with open(log_cfg_path, 'r') as f:
+#     dict_cfg = yaml.load(f, Loader=yaml.FullLoader)
+# logging.config.dictConfig(dict_cfg)
+# logger = logging.getLogger("mylogger")
+
+class Trainer:
+    def __init__(self, config):
+        self.gd = GenerateData(config)
+
+        self.train_data, cat_num = self.gd.get_train_data()
+        valid_data = self.gd.get_val_data(config['valid_num'])
+        self.model = MyModel(ResNet_v1_50, embedding_size=config['embedding_size'], classes=cat_num)
+        self.epoch_num = config['epoch_num']
+        self.m1 = config['logits_margin1']
+        self.m2 = config['logits_margin2']
+        self.m3 = config['logits_margin3']
+        self.s = config['logits_scale']
+        self.alpha = config['alpha']
+        self.thresh = config['thresh']
+        self.below_fpr = config['below_fpr']
+        self.learning_rate = config['learning_rate']
+        self.loss_type = config['loss_type']
+
+        # center loss init
+        self.centers = None
+        self.ct_loss_factor = config['center_loss_factor']
+        self.ct_alpha = config['center_alpha']
+        if self.loss_type == 'logit' and self.ct_loss_factor > 0:
+            self.centers = tf.Variable(initial_value=tf.zeros((cat_num, config['embedding_size'])), trainable=False)
+
+        optimizer = config['optimizer']
+        if optimizer == 'ADADELTA':
+            self.optimizer = tf.keras.optimizers.Adadelta(self.learning_rate)
+        elif optimizer == 'ADAGRAD':
+            self.optimizer = tf.keras.optimizers.Adagrad(self.learning_rate)
+        elif optimizer == 'ADAM':
+            self.optimizer = tf.keras.optimizers.Adam(self.learning_rate)
+        elif optimizer == 'ADAMAX':
+            self.optimizer = tf.keras.optimizers.Adamax(self.learning_rate)
+        elif optimizer == 'FTRL':
+            self.optimizer = tf.keras.optimizers.Ftrl(self.learning_rate)
+        elif optimizer == 'NADAM':
+            self.optimizer = tf.keras.optimizers.Nadam(self.learning_rate)
+        elif optimizer == 'RMSPROP':
+            self.optimizer = tf.keras.optimizers.RMSprop(self.learning_rate)
+        elif optimizer == 'SGD':
+            self.optimizer = tf.keras.optimizers.SGD(self.learning_rate)
+        else:
+            raise ValueError('Invalid optimization algorithm')
+
+        ckpt_dir = os.path.expanduser(config['ckpt_dir'])
+
+        if self.centers is None:
+            self.ckpt = tf.train.Checkpoint(backbone=self.model.backbone, model=self.model, optimizer=self.optimizer)
+        else:
+            # save centers if use center loss
+            self.ckpt = tf.train.Checkpoint(backbone=self.model.backbone, model=self.model, optimizer=self.optimizer,
+                                            centers=self.centers)
+        self.ckpt_manager = tf.train.CheckpointManager(self.ckpt, ckpt_dir, max_to_keep=5, checkpoint_name='mymodel')
+
+        if self.ckpt_manager.latest_checkpoint:
+            self.ckpt.restore(self.ckpt_manager.latest_checkpoint)
+            print("Restored from {}".format(self.ckpt_manager.latest_checkpoint))
+        else:
+            print("Initializing from scratch.")
+
+        self.vd = Valid_Data(self.model, valid_data)
+
+        summary_dir = os.path.expanduser(config['summary_dir'])
+        current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+        train_log_dir = os.path.join(summary_dir, current_time, 'train')
+        valid_log_dir = os.path.join(summary_dir, current_time, 'valid')
+        # self.graph_log_dir = os.path.join(summary_dir, current_time, 'graph')
+
+        if platform.system() == 'Windows':
+            train_log_dir = train_log_dir.replace('/', '\\')
+            valid_log_dir = valid_log_dir.replace('/', '\\')
+            # self.graph_log_dir = self.graph_log_dir.replace('/', '\\')
+
+        # self.train_summary_writer = tf.summary.create_file_writer(train_log_dir)
+        # self.valid_summary_writer = tf.summary.create_file_writer(valid_log_dir)
+        self.train_summary_writer = tf.compat.v2.summary.create_file_writer(train_log_dir)
+        self.valid_summary_writer = tf.compat.v2.summary.create_file_writer(valid_log_dir)
+
+        # self.graph_writer = tf.compat.v2.summary.create_file_writer(self.graph_log_dir)
+        # tf.compat.v2.summary.trace_on(graph=True, profiler=True)
+        # with graph_writer.as_default():
+        #     tf.compat.v2.summary.trace_export(name="graph_trace", step=0, profiler_outdir=graph_log_dir)
+
+    @tf.function
+    def _train_step(self, img, label):
+        with tf.GradientTape(persistent=False) as tape:
+            prelogits, dense, norm_dense = self.model(img, training=True)
+
+            # sm_loss = softmax_loss(dense, label)
+            # norm_sm_loss = softmax_loss(norm_dense, label)
+            arc_loss = arcface_loss(prelogits, norm_dense, label, self.m1, self.m2, self.m3, self.s)
+            logit_loss = arc_loss
+
+            if self.centers is not None:
+                ct_loss, self.centers = center_loss(prelogits, label, self.centers, self.ct_alpha)
+            else:
+                ct_loss = 0
+
+            loss = logit_loss + self.ct_loss_factor * ct_loss
+        gradients = tape.gradient(loss, self.model.trainable_variables)
+        self.optimizer.apply_gradients(zip(gradients, self.model.trainable_variables))
+
+        return loss, logit_loss, ct_loss
+
+    @tf.function
+    def _train_triplet_step(self, anchor, pos, neg):
+        with tf.GradientTape(persistent=False) as tape:
+            anchor_emb = get_embeddings(self.model, anchor)
+            pos_emb = get_embeddings(self.model, pos)
+            neg_emb = get_embeddings(self.model, neg)
+
+            loss = triplet_loss(anchor_emb, pos_emb, neg_emb, self.alpha)
+
+        gradients = tape.gradient(loss, self.model.trainable_variables)
+        self.optimizer.apply_gradients(zip(gradients, self.model.trainable_variables))
+
+        return loss
+
+    def train(self):
+        for epoch in range(self.epoch_num):
+            start = time.time()
+            # triplet loss
+            if self.loss_type == 'triplet':
+                train_data, num_triplets = self.gd.get_train_triplets_data(self.model)
+                print('triplets num is {}'.format(num_triplets))
+                if num_triplets > 0:
+                    for step, (anchor, pos, neg) in enumerate(train_data):
+                        loss = self._train_triplet_step(anchor, pos, neg)
+                        with self.train_summary_writer.as_default():
+                            tf.compat.v2.summary.scalar('loss', loss, step=step)
+                        print('epoch: {}, step: {}, loss = {}'.format(epoch, step, loss))
+            elif self.loss_type == 'logit':
+                # logit loss
+                for step, (input_image, target) in enumerate(self.train_data):
+                    loss, logit_loss, ct_loss = self._train_step(input_image, target)
+                    with self.train_summary_writer.as_default():
+                        tf.compat.v2.summary.scalar('loss', loss, step=step)
+                        tf.compat.v2.summary.scalar('logit_loss', logit_loss, step=step)
+                        tf.compat.v2.summary.scalar('center_loss', ct_loss, step=step)
+                    print('epoch: {}, step: {}, loss = {}, logit_loss = {}, center_loss = {}'.format(epoch, step, loss,
+                                                                                                     logit_loss,
+                                                                                                     ct_loss))
+            else:
+                raise ValueError('Invalid loss type')
+
+            # valid
+            acc, p, r, fpr, acc_fpr, p_fpr, r_fpr, thresh_fpr = self.vd.get_metric(self.thresh, self.below_fpr)
+
+            with self.valid_summary_writer.as_default():
+                tf.compat.v2.summary.scalar('acc', acc, step=epoch)
+                tf.compat.v2.summary.scalar('p', p, step=epoch)
+                tf.compat.v2.summary.scalar('r=tpr', r, step=epoch)
+                tf.compat.v2.summary.scalar('fpr', fpr, step=epoch)
+                tf.compat.v2.summary.scalar('acc_fpr', acc_fpr, step=epoch)
+                tf.compat.v2.summary.scalar('p_fpr', p_fpr, step=epoch)
+                tf.compat.v2.summary.scalar('r=tpr_fpr', r_fpr, step=epoch)
+                tf.compat.v2.summary.scalar('thresh_fpr', thresh_fpr, step=epoch)
+            print('epoch: {}, acc: {:.3f}, p: {:.3f}, r=tpr: {:.3f}, fpr: {:.3f} \n'
+                  'fix fpr <= {}, acc: {:.3f}, p: {:.3f}, r=tpr: {:.3f}, thresh: {:.3f}'
+                  .format(epoch, acc, p, r, fpr, self.below_fpr, acc_fpr, p_fpr, r_fpr, thresh_fpr))
+
+            # ckpt
+            # if epoch % 5 == 0:
+            save_path = self.ckpt_manager.save()
+            print('Saving checkpoint for epoch {} at {}'.format(epoch, save_path))
+
+            print('Time taken for epoch {} is {} sec\n'.format(epoch, time.time() - start))
+
+
+def parse_args(argv):
+    parser = argparse.ArgumentParser(description='Train face network')
+    parser.add_argument('--config_path', type=str, help='path to config path', default='configs/config.yaml')
+
+    args = parser.parse_args(argv)
+
+    return args
+
+
+def main():
+    args = parse_args(sys.argv[1:])
+    # logger.info(args)
+
+    with open(args.config_path) as cfg:
+        config = yaml.load(cfg, Loader=yaml.FullLoader)
+
+    t = Trainer(config)
+    t.train()
+
+
+if __name__ == '__main__':
+    # logger.info("hello, insightface/recognition")
+    main()
