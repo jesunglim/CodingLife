47e2da10b997d4820ca2160d1b370746e04939a5
jesung <jesunglimkorea@gmail.com>
2022-03-22 14:06:58 +0900

    PyTorch Tutorial

---
+++ 20220322/model_loader.py
@@ -0,0 +1,34 @@
+from numpy import dtype
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor, Lambda, Compose
+import matplotlib.pyplot as plt
+
+import torch_tutorial
+
+
+
+model = torch_tutorial.NeuralNetwork()
+model.load_state_dict(torch.load("model.pth"))
+
+classes = [
+    "T-shirt/top",
+    "Trouser",
+    "Pullover",
+    "Dress",
+    "Coat",
+    "Sandal",
+    "Shirt",
+    "Sneaker",
+    "Bag",
+    "Ankle boot",
+]
+
+model.eval()
+x, y = torch_tutorial.test_data[0][0], torch_tutorial.test_data[0][1]
+with torch.no_grad():
+    pred = model(x)
+    predicted, actual = classes[pred[0].argmax(0)], classes[y]
+    print(f'Predcted: "{predicted}", Actual: "{actual}"')
