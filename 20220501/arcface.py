782c09f2d4dead98f9834b2ae7a8d6d27b0dbd9f
jesunglim <jesunglimkorea@gmail.com>
2022-05-02 00:47:21 +0900

    torch training

---
+++ 20220501/arcface.py
@@ -0,0 +1,195 @@
+# fuck
+
+import os
+from PIL import Image
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torch import nn
+from torchvision import transforms
+
+import math
+from torch.nn import Parameter
+import torch.nn.functional as F
+
+import numpy as np
+import time
+
+
+class CustomImageDataset(Dataset):
+    def read_data_set(self):
+
+        all_img_files = []
+        all_labels = []
+        img_size = 0
+
+        class_names = os.walk(self.data_set_path).__next__()[1]
+
+        for index, class_name in enumerate(class_names):
+            label = index
+            img_dir = os.path.join(self.data_set_path, class_name)
+            img_files = os.walk(img_dir).__next__()[2]
+
+            for img_file in img_files:
+                img_file = os.path.join(img_dir, img_file)
+                img = Image.open(img_file)
+                img_size = img.size
+                if img is not None:
+                    all_img_files.append(img_file)
+                    all_labels.append(label)
+
+        return all_img_files, all_labels, len(all_img_files), len(class_names), img_size
+
+    def __init__(self, data_set_path, transforms=None):
+        self.data_set_path = data_set_path
+        self.image_files_path, self.labels, self.length, self.num_classes, self.img_size = self.read_data_set()
+        self.transforms = transforms
+
+    def __getitem__(self, index):
+        image = Image.open(self.image_files_path[index])
+        image = image.convert("RGB")
+
+        if self.transforms is not None:
+            image = self.transforms(image)
+
+        return {'image': image, 'label': self.labels[index]}
+
+    def __len__(self):
+        return self.length
+
+class CustomConvNet(nn.Module):
+    def __init__(self, num_classes):
+        super(CustomConvNet, self).__init__()
+
+        self.layer1 = self.conv_module(3, 16)
+        self.layer2 = self.conv_module(16, 32)
+        self.layer3 = self.conv_module(32, 64)
+        self.layer4 = self.conv_module(64, 128)
+        self.layer5 = self.conv_module(128, 256)
+        self.gap = self.global_avg_pool(256, num_classes)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.layer5(out)
+        out = self.gap(out)
+        out = out.view(-1, num_classes)
+
+        return out
+
+    def conv_module(self, in_num, out_num):
+        return nn.Sequential(
+            nn.Conv2d(in_num, out_num, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(out_num),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+
+    def global_avg_pool(self, in_num, out_num):
+        return nn.Sequential(
+            nn.Conv2d(in_num, out_num, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(out_num),
+            nn.LeakyReLU(),
+            nn.AdaptiveAvgPool2d((1, 1)))
+
+class ArcMarginProduct(nn.Module):
+    def __init__(self, in_feature, out_feature, s=32.0, m=0.50, easy_margin=False):
+        super(ArcMarginProduct, self).__init__()
+        self.in_feature = in_feature
+        self.out_feature = out_feature
+        self.s = s
+        self.m = m
+        self.weight = Parameter(torch.Tensor(out_feature, in_feature))
+        nn.init.xavier_uniform_(self.weight)
+
+        self.easy_margin = easy_margin
+        self.cos_m = math.cos(m)
+        self.sin_m = math.sin(m)
+
+        # make the function cos(theta+m) monotonic decreasing while theta in [0°,180°]
+        self.th = math.cos(math.pi - m)
+        self.mm = math.sin(math.pi - m) * m
+
+    def forward(self, x, label):
+        # cos(theta)
+        cosine = F.linear(F.normalize(x), F.normalize(self.weight))
+        # cos(theta + m)
+        sine = torch.sqrt(1.0 - torch.pow(cosine, 2))
+        phi = cosine * self.cos_m - sine * self.sin_m
+
+        if self.easy_margin:
+            phi = torch.where(cosine > 0, phi, cosine)
+        else:
+            phi = torch.where((cosine - self.th) > 0, phi, cosine - self.mm)
+
+        #one_hot = torch.zeros(cosine.size(), device='cuda' if torch.cuda.is_available() else 'cpu')
+        one_hot = torch.zeros_like(cosine)
+        one_hot.scatter_(1, label.view(-1, 1), 1)
+        output = (one_hot * phi) + ((1.0 - one_hot) * cosine)
+        output = output * self.s
+
+        return output
+
+
+hyper_param_epoch = 20
+hyper_param_batch = 8
+hyper_param_learning_rate = 0.001
+resize = 128
+
+transforms_train = transforms.Compose([transforms.Resize((resize, resize)),
+                                       transforms.RandomRotation(10.),
+                                       transforms.ToTensor()])
+
+transforms_test = transforms.Compose([transforms.Resize((resize, resize)),
+                                      transforms.ToTensor()])
+
+train_data_set = CustomImageDataset(data_set_path="C:/Users/jesung/Desktop/train", transforms=transforms_train)
+train_loader = DataLoader(train_data_set, batch_size=hyper_param_batch, shuffle=True)
+
+test_data_set = CustomImageDataset(data_set_path="C:/Users/jesung/Desktop/test", transforms=transforms_test)
+test_loader = DataLoader(test_data_set, batch_size=hyper_param_batch, shuffle=True)
+
+if not (train_data_set.num_classes == test_data_set.num_classes):
+    print("error: Numbers of class in training set and test set are not equal")
+    exit()
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+num_classes = train_data_set.num_classes
+custom_model = CustomConvNet(num_classes=num_classes).to(device)
+feature_dim = resize
+print(feature_dim)
+arcface = ArcMarginProduct(in_feature=feature_dim, out_feature=num_classes, s=feature_dim).to(device)
+
+
+
+
+# Loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(custom_model.parameters(), lr=hyper_param_learning_rate)
+
+total_iters = 0
+for e in range(hyper_param_epoch):
+    for i_batch, item in enumerate(train_loader):
+        images = item['image'].to(device)
+        labels = item['label'].to(device)
+        optimizer.zero_grad()
+
+        raw_logits = custom_model(images)
+        output = arcface(raw_logits, labels)
+        loss = criterion(output, labels)
+        loss.backward()
+        optimizer.step()
+
+        total_iters += 1
+        # print train information
+        if total_iters % 100 == 0:
+            # current training accuracy
+            _, predict = torch.max(output.data, 1)
+            total = labels.size(0)
+            correct = (np.array(predict.cpu()) == np.array(label.data.cpu())).sum()
+            time_cur = (time.time() - since) / 100
+            since = time.time()
+
+            print("Iters: {:0>6d}/[{:0>2d}], loss: {:.4f}, train_accuracy: {:.4f}, time: {:.2f} s/iter".format(total_iters, e, loss.item(), correct / total, time_cur))
